00fdce3a59830fdd01cd6526cb56d307fdc0bd17
anurag <anurag.phadke@gmail.com>
2023-12-08 13:54:18 -0800

    a

--- index.js
+++ index.js
@@ -15,8 +15,19 @@ const screenshot = promisify((videoStream, time, outputPath, filename, callback)
         });
 });
 
+function secondsToTimeFormat(seconds) {
+    const pad = (num, size) => ('000' + num).slice(size * -1);
+    const time = parseFloat(seconds).toFixed(3);
+    const hours = Math.floor(time / 60 / 60);
+    const minutes = Math.floor(time / 60) % 60;
+    const secs = Math.floor(time - minutes * 60);
+
+    return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(secs, 2);
+}
+
 async function takeScreenshot(videoUrl, time, outputPath, filename) {
     try {
+        time = secondsToTimeFormat(time)
         const videoStream = ytdl(videoUrl, { quality: 'highest' });
         await screenshot(videoStream, time, outputPath, filename);
     } catch (error) {
